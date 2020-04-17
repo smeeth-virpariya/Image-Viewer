0a7664e3f0f64cab6bae21bcfd4a11e990345c08
Smeeth Virpariya <smeethvirparia@yahoo.co.in>
2020-04-17 22:30:50 +0530

    Added menu to the profile icon button.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,13 +3,21 @@ import React, {Component, Fragment} from 'react';
 //Import of stylesheet for header component.
 import './Header.css';
 
-import {Avatar, IconButton, Input, InputAdornment} from "@material-ui/core";
+import {Avatar, IconButton, Input, InputAdornment, Menu, MenuItem, Typography} from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 /**
  * Header Component serves header for multiple pages.
  */
 class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            menuState: false,
+            anchorEl: null
+        }
+    }
 
     render() {
         return <div className='header-flex-container'>
@@ -28,16 +36,32 @@ class Header extends Component {
                                    startAdornment={
                                        <InputAdornment position="start"><SearchIcon/></InputAdornment>
                                    }/>
-                            <IconButton>
+                            <IconButton id='profile-icon' onClick={this.onProfileIconClick}>
                                 <Avatar alt="profile_picture"
                                         src={this.props.profilePictureUrl}/>
                             </IconButton>
+                            <div>
+                                <Menu open={this.state.menuState} onClose={this.onMenuClose}
+                                      anchorEl={this.state.anchorEl} getContentAnchorEl={null}
+                                      anchorOrigin={{vertical: "bottom", horizontal: "right"}} keepMounted>
+                                    <MenuItem><Typography>My Account</Typography></MenuItem>
+                                    <hr className='horizontal-line'/>
+                                    <MenuItem><Typography>Logout</Typography></MenuItem>
+                                </Menu>
+                            </div>
                         </div>
                     </Fragment>
             }
         </div>
 
+    }
 
+    onProfileIconClick = (e) => {
+        this.setState({'menuState': !this.state.menuState, 'anchorEl': e.currentTarget});
+    }
+
+    onMenuClose = () => {
+        this.setState({'menuState': !this.state.menuState, 'anchorEl': null});
     }
 }
 
