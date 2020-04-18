09def7f4cdf2d26980d47aa8fcb5290ee0e316b2
Smeeth Virpariya <smeethvirparia@yahoo.co.in>
2020-04-18 18:02:00 +0530

    Adjusted some styling for home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
 import Header from "../../common/header/Header";
 
+import './Home.css'
+
 //Router import for redirection.
 import {Redirect} from 'react-router-dom';
+
+import {
+    Avatar,
+    Button,
+    Card,
+    CardContent,
+    CardHeader,
+    FormControl,
+    GridList,
+    Input,
+    InputLabel
+} from '@material-ui/core'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 class Home extends Component {
 
@@ -10,22 +25,61 @@ class Home extends Component {
         super();
         this.baseUrl = 'https://api.instagram.com/v1/';
         this.state = {
-            profile_picture: ''
+            profile_picture: '',
+            recent_media: null
         }
     }
 
     componentDidMount() {
         this.fetchOwnerInfo();
+        this.fetchMostRecentMedia();
     }
 
     render() {
+        console.log(this.state.recent_media)
         if (this.props.location.state === undefined) {
             return <Redirect to='/'/>
         }
         if (this.props.location.state.loginSuccess === true) {
             return <div>
                 <div><Header isLoggedIn={true} profilePictureUrl={this.state.profile_picture}/></div>
-                <div><span>Success</span></div>
+                <div className='posts-card-container'>
+                    {
+                        (this.state.recent_media || []).map((details, index) => (
+                            <Card key={details.id} className='post-card'>
+                                <CardHeader avatar={<Avatar src={details.user.profile_picture}/>}
+                                            title={details.user.username}
+                                            subheader={new Date(details.created_time * 1000).toLocaleString()}/>
+                                <CardContent>
+                                    <img className='post-image' src={details.images.standard_resolution.url}/>
+                                    <hr/>
+                                    <div className='post-caption'>{details.caption.text.split("\n")[0]}</div>
+                                    {details.tags.map((tag, index) => (
+                                        <span key={index}><a className='post-tags'
+                                                             href={tag}>{'#' + tag + ' '}</a></span>)
+                                    )}
+                                    <br/>
+                                    <div className='likes'>
+                                        <FavoriteBorderIcon fontSize='default'/>
+                                        <pre> </pre>
+                                        <span>{details.likes.count + ' likes'}</span>
+                                    </div>
+                                    <div className='post-comment'>
+                                        <FormControl className='post-comment-form-control'>
+                                            <InputLabel htmlFor='comment'>Add a comment</InputLabel>
+                                            <Input className='comment-input' type='text'></Input>
+                                        </FormControl>
+                                        <div className='add-button'>
+                                            <FormControl>
+                                                <Button variant='contained' color='primary'>ADD</Button>
+                                            </FormControl>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
+                </div>
             </div>
         }
     }
@@ -43,7 +97,27 @@ class Home extends Component {
             }
         });
 
-        let url = this.baseUrl+"users/self/?access_token=" + sessionStorage.getItem("access-token");
+        let url = this.baseUrl + "users/self/?access_token=" + sessionStorage.getItem("access-token");
+
+        xhr.open("GET", url);
+
+        xhr.send(data);
+    }
+
+    fetchMostRecentMedia = () => {
+        let data = null;
+
+        let xhr = new XMLHttpRequest();
+
+        let that = this;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({recent_media: JSON.parse(this.responseText).data});
+            }
+        });
+
+        let url = this.baseUrl + "users/self/media/recent/?access_token=" + sessionStorage.getItem("access-token");
 
         xhr.open("GET", url);
 
