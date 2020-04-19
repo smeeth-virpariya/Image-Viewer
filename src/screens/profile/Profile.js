5435dbff272467c3e9b95f3d252710b3e1a6469c
Smeeth Virpariya <smeethvirparia@yahoo.co.in>
2020-04-19 23:41:31 +0530

    Added all pictures in grid

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -3,7 +3,7 @@ import Header from '../../common/header/Header';
 import {Redirect} from 'react-router-dom';
 import './Profile.css'
 
-import {Avatar, Container, Fab, Typography, Grid} from '@material-ui/core/';
+import {Avatar, Card, CardMedia,CardContent, Container, Fab,GridList,GridListTile, Typography, Grid} from '@material-ui/core/';
 
 import EditIcon from '@material-ui/icons/Edit'
 
@@ -30,12 +30,12 @@ class Profile extends Component {
             return <Redirect to='/'/>
         } else if (this.props.location.state.loginSuccess === true) {
             console.log(this.state.recent_media)
-            return <div className='main_container'>
+            return <div>
                 <Header {...this.props} isLoggedIn={true} showSearchBox={false}
                         profilePictureUrl={this.state.profile_picture} showMyAccount={false}/>
                 <Container>
                     <div style={{height: "2rem"}}></div>
-                    <Grid container spacing={3} justify="flex-start" >
+                    <Grid container spacing={3} justify="flex-start">
                         <Grid item xs={2}/>
                         <Grid item xs={2}>
                             {this.state.profile_picture ? (
@@ -44,12 +44,12 @@ class Profile extends Component {
                                     id="profile-pic"
                                     variant="circle"
                                     src={this.state.profile_picture}
-                                    style={{ marginTop: 10 }}
+                                    style={{marginTop: 10}}
                                 />
                             ) : null}
                         </Grid>
                         <Grid item xs={4} id='user_name'>
-                            <Typography variant="h4" component="h1" style={{ marginBottom: 5 }}>
+                            <Typography variant="h4" component="h1" style={{marginBottom: 5}}>
                                 {this.state.recent_media
                                     ? this.state.user_data.username
                                     : null}
@@ -68,13 +68,13 @@ class Profile extends Component {
                                         : null}
                                 </Grid>
                                 <Grid item xs={4}>
-                                    Follows By:{" "}
+                                    Followed By:{" "}
                                     {this.state.user_data
                                         ? this.state.user_data.counts.followed_by
                                         : null}
                                 </Grid>
                             </Grid>
-                            <Typography variant="h6" component="h2" style={{ marginTop: 5 }}>
+                            <Typography variant="h6" component="h2" style={{marginTop: 5}}>
                                 {this.state.user_data ? this.state.user_data.full_name : null}
                                 {this.state.user_data && !this.state.user_data.full_name
                                     ? this.state.user_data.full_name
@@ -89,6 +89,24 @@ class Profile extends Component {
                             </Typography>
                         </Grid>
                         <Grid item xs={4}/>
+                    </Grid>
+                </Container>
+                <Container>
+                    <Grid container spacing={1} direction="row" alignItems="center">
+                        {this.state.recent_media &&
+                        this.state.recent_media.map((mediaObj, index) => (
+                            <Grid
+                                // id="media-grids"
+                                item
+                                xs={4}
+                                key={mediaObj.id}>
+                                <Card variant="outlined">
+                                    <CardMedia style={{height: 0,paddingTop: '56.25%'}}
+                                        image={mediaObj.images.standard_resolution.url}
+                                        title={mediaObj.images.standard_resolution.url}/>
+                                </Card>
+                            </Grid>
+                        ))}
                     </Grid>
                 </Container>
             </div>
